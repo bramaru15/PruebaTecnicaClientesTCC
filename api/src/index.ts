6c9f688f62fdf886835a98c02a4bf8bb56e5644c
Yuzmel <Yamm1995@outlook.com>
2023-06-27 11:45:38 -0500

    api prefix

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
-app.use(Router);
+app.use('/api', Router);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
