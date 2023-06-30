3dcc2dc88f5c1a02ea5f522e67027b20abe95b4f
Yuzmel <Yamm1995@outlook.com>
2023-06-29 23:59:10 -0500

    api done

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,15 +2,26 @@ import express, { Application } from "express";
 import morgan from "morgan";
 import Router from "./routes";
 import 'dotenv/config';
+import cors from "cors";
+ 
 
-const PORT = process.env.PORT || 8000;
+
+const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = ['http://localhost:4200'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 
 const app: Application = express();
 
+app.use(cors(options));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
 app.use('/api', Router);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
